84a001207869bb9670df62367e2095246d26c809
Kit-p <65013409+Kit-p@users.noreply.github.com>
2020-12-09 02:53:25 +0800

    Implement MakeMoveByBehavior.getNextMove()

--- src/main/java/castle/comp3021/assignment/protocol/MakeMoveByBehavior.java
+++ src/main/java/castle/comp3021/assignment/protocol/MakeMoveByBehavior.java
@@ -2,6 +2,9 @@ package castle.comp3021.assignment.protocol;
 
 import castle.comp3021.assignment.piece.Knight;
 
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class MakeMoveByBehavior {
     private final Behavior behavior;
     private final Game game;
@@ -27,7 +30,105 @@ public class MakeMoveByBehavior {
      */
     public Move getNextMove(){
         // TODO
-        return this.availableMoves[0];
+        return switch (this.behavior) {
+            case GREEDY -> getGreedyMove();
+            case CAPTURING -> getCapturingMove();
+            case BLOCKING -> getBlockingMove();
+            default -> getRandomMove();
+        };
+    }
+
+    /**
+     * @return move chosen by RANDOM behavior
+     */
+    private Move getRandomMove() {
+        if (availableMoves.length <= 0) {
+            return null;
+        }
+        return availableMoves[new Random().nextInt(availableMoves.length)];
+    }
+
+    /**
+     * @return move chosen by GREEDY behavior
+     */
+    private Move getGreedyMove() {
+        List<Move> moves = new ArrayList<>(Arrays.asList(availableMoves));
+        return moves.stream()
+                .filter(this::isGreedyMove)
+                .min(Comparator.comparing(move -> getManhattanDistance(move.getDestination(), game.getCentralPlace())))
+                .orElseGet(this::getRandomMove);
+    }
+
+    /**
+     * @return move chosen by CAPTURING behavior
+     */
+    private Move getCapturingMove() {
+        List<Move> moves = new ArrayList<>(Arrays.asList(availableMoves));
+        moves = moves.stream()
+                .filter(this::isCapturingMove)
+                .collect(Collectors.toList());
+        if (moves.size() <= 0) {
+            return getRandomMove();
+        }
+        return moves.get(new Random().nextInt(moves.size()));
+    }
+
+    /**
+     * @return move chosen by BLOCKING behavior
+     */
+    private Move getBlockingMove() {
+        List<Move> moves = new ArrayList<>(Arrays.asList(availableMoves));
+        moves = moves.stream()
+                .filter(this::isBlockingMove)
+                .collect(Collectors.toList());
+        if (moves.size() <= 0) {
+            return getRandomMove();
+        }
+        return moves.get(new Random().nextInt(moves.size()));
+    }
+
+    private int getManhattanDistance(Place source, Place destination) {
+        if (source == null || destination == null) {
+            return Integer.MAX_VALUE;
+        }
+        return Math.abs(destination.x() - source.x()) + Math.abs(destination.y() - source.y());
+    }
+
+    private boolean isGreedyMove(Move move) {
+        if (move == null) {
+            return false;
+        }
+        int oldDistance = getManhattanDistance(move.getSource(), game.getCentralPlace());
+        int newDistance = getManhattanDistance(move.getDestination(), game.getCentralPlace());
+        return newDistance < oldDistance;
+    }
+
+    private boolean isCapturingMove(Move move) {
+        if (move == null) {
+            return false;
+        }
+        Piece piece = game.getPiece(move.getDestination());
+        return !(piece == null || piece.getPlayer().equals(game.currentPlayer));
+    }
+
+    private boolean isBlockingMove(Move move) {
+        if (move == null) {
+            return false;
+        }
+        Place destination = move.getDestination();
+        int[] offsets = {1, -1};
+        for (int offset : offsets) {
+            Piece pieceX = game.getPiece(destination.x() + offset, destination.y());
+            Piece pieceY = game.getPiece(destination.x(), destination.y() + offset);
+            if (isEnemyKnight(pieceX) || isEnemyKnight(pieceY)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isEnemyKnight(Piece piece) {
+        return (piece instanceof Knight && !(piece.getPlayer().equals(game.currentPlayer)));
     }
 }
 
